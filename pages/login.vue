4b2266c440164c02be50119ec4e1096de9241053
ashish100 <er.mr.ashish@gmail.com>
2025-02-18 19:25:39 +0530

    NEW FILES

---
+++ pages/login.vue
@@ -0,0 +1,25 @@
+<template>
+ 
+  <div class="mt-5">
+      <h2>Login Page</h2>
+ 
+      <form @submit.prevent="submitLoginForm">
+         
+        <input type="email" v-model="member.apiemail" placeholder="Email"   />
+        <input type="password" v-model="member.apipassword" placeholder="Password"  /> 
+        <button type="submit">Login</button>
+      </form>
+ </div>
+</template>
+  
+<script setup>
+  import { ref } from 'vue';
+       
+      const member = ref({ apiemail: '', apipassword:'' });
+      const router = useRouter();
+
+      const submitLoginForm = async () => {
+ 
+    };
+  
+</script>
